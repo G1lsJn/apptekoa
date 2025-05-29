b6fa92238940ef65c853f6990a6c038379c0927f
Gilson Jr <gilsondiasjn15@gmail.com>
2025-05-29 20:04:52 -0300

    Entidades da gs - queimadas

--- src/main/java/br/com/tekoa/apptekoa/services/AnimalService.java
+++ src/main/java/br/com/tekoa/apptekoa/services/AnimalService.java
@@ -1,5 +1,6 @@
 package br.com.tekoa.apptekoa.services;
 
+import br.com.tekoa.apptekoa.entities.Animal;
 import br.com.tekoa.apptekoa.repositories.AnimalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ public class AnimalService {
 
     @Autowired
     private AnimalRepository animalRepository;
+
+    public Iterable<Animal> findAll(){
+
+    }
 }
